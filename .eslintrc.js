3d8927f889694c782cf9b17791fa4c6b8b31b621
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2021-04-13 16:04:18 +0300

    добавил кастомные правила в eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'vue/no-unused-components': "off",
+    'quotes': ["error", "double"]
   },
   overrides: [
     {
