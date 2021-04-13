596579db5f8e676403ca14b4d8fe0062804e04c5
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2021-04-13 20:14:30 +0300

    добавил элемент линейки сториз

---
+++ src/components/storyUserItem/storyUserItem.vue
@@ -0,0 +1,34 @@
+<template>
+ <div class="c-story-user-item">
+   <div class="avatar">
+     <avatar
+       :src="src"
+       :username="username"
+     />
+   </div>
+   <div class="username">{{username}}</div>
+ </div>
+</template>
+
+<script>
+import { avatar } from "../avatar";
+
+export default {
+  components: {
+    avatar
+  },
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true
+    }
+  }
+};
+
+</script>
+
+<style lang="scss" scoped src="./storyUserItem.scss"></style>
