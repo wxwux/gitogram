727b7c12305e0beaf5e5345a1e2d749cb1dc168f
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2021-04-27 17:02:03 +0300

    добавил параметр языка в запрос трендов

--- src/api/rest/trendings.js
+++ src/api/rest/trendings.js
@@ -1,13 +1,11 @@
 import { makeRequest } from "../requests";
 
-// curl -G https://api.github.com/search/repositories --data-urlencode "sort=stars" --data-urlencode "order=desc" --data-urlencode "q=language:java"  --data-urlencode "q=created:>`date -v-7d '+%Y-%m-%d'`"
-
 export const getTrendings = (lang = "javascript", period = "weekly") => {
   const params = new URLSearchParams();
 
   params.append("sort", "stars");
   params.append("order", "desc");
-  params.append("q", "language:javascript created:>2021-04-20");
+  params.append("q", `language:${lang} created:>2021-04-20`);
   params.append("per_page", 10);
 
   return makeRequest({
