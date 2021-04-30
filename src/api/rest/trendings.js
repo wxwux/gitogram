fe9398f4131b11bc802cc5b35bd6e29b16c4b0c7
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2021-04-30 16:57:25 +0300

    вывел данные в слайдер

--- src/api/rest/trendings.js
+++ src/api/rest/trendings.js
@@ -1,11 +1,21 @@
 import { makeRequest } from "../requests";
 
+const addStaringZero = (value) => (value < 10 ? `0${value}` : value);
+
 export const getTrendings = (lang = "javascript", period = "weekly") => {
   const params = new URLSearchParams();
+  const weekS = 7 * 24 * 60 * 60 * 1000;
+  const weekAgo = new Date(Date.now() - weekS);
+
+  const formattedDate = [
+    weekAgo.getFullYear(),
+    addStaringZero(weekAgo.getMonth() + 1),
+    addStaringZero(weekAgo.getDate())
+  ].join("-");
 
   params.append("sort", "stars");
   params.append("order", "desc");
-  params.append("q", `language:${lang} created:>2021-04-20`);
+  params.append("q", `language:${lang} created:>${formattedDate}`);
   params.append("per_page", 10);
 
   return makeRequest({
---
+++ src/components/storiesSlider/storiesSlider.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="c-stories-slider">
+    <div class="x-container">
+      <div class="stories-container">
+        <ul
+          class="stories"
+          ref="slider"
+        >
+          <li
+            class="stories-item"
+            v-for="({
+              id, owner, name, readme, following
+            }, ndx) in trendings"
+            :key="id"
+          >
+            <story-post-item
+              :user-avatar="owner.avatar_url"
+              :username="owner.login"
+              :content="readme"
+              :active="ndx === index"
+              :following="following"
+              :buttons-shown="activeBtns"
+              @next="moveSlide('next')"
+              @prev="moveSlide('prev')"
+              @followTheRepo="starRepo({id, owner: owner.login, repo: name})"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+import { storyPostItem } from "../storyPostItem";
+
+export default {
+  components: { storyPostItem },
+  data() {
+    return {
+      index: 0,
+      sliderPosition: 0
+    };
+  },
+  computed: {
+    ...mapState({
+      trendings: (state) => state.trendings.data
+    }),
+    activeBtns() {
+      if (this.index === 0) return ["next"];
+      if (this.index === this.trendings.length - 1) return ["prev"];
+      return ["next", "prev"];
+    },
+    activeSlideData() {
+      const { id, owner, name } = this.trendings[this.index];
+      return { id, owner: owner.login, repo: name };
+    },
+  },
+  methods: {
+    ...mapActions({
+      fetchTrendings: "trendings/fetchTrendings",
+      fetchReadme: "trendings/fetchReadmeForRepo",
+      starRepo: "trendings/starRepo"
+    }),
+    moveSlide(direction) {
+      const { slider } = this.$refs;
+
+      switch (direction) {
+        case "next":
+          this.index += 1;
+          this.sliderPosition -= 300;
+          break;
+        case "prev":
+          this.index -= 1;
+          this.sliderPosition += 300;
+          break;
+        default: this.index += 1;
+      }
+
+      slider.style.transform = `translateX(${this.sliderPosition}px)`;
+
+      const { id, owner, repo } = this.activeSlideData;
+      this.fetchReadme({ id, owner, repo });
+    }
+  },
+  mounted() {
+    this.fetchTrendings();
+  },
+};
+
+</script>
+
+<style lang="scss" scoped src="./storiesSlider.scss"></style>
