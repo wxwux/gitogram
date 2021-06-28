f4b4cc6921b42115a7c3d3de3bd4832e074d8b7f
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2021-06-28 18:21:43 +0300

    добавил прелоадер для ишьюз

---
+++ src/components/contentLoader/contentLoader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="c-content-loader">
+    <div class="item" v-for="n in 3" :key="n"></div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped src="./contentLoader.scss"></style>
